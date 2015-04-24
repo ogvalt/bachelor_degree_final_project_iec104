33c476257542bcb8db518c5060b584db245bf642
yyk0205 <yyk0205@gmail.com>
2015-04-24 13:03:18 +0300

    APCI.h

--- APCI.cpp
+++ APCI.cpp
@@ -1,10 +1,6 @@
-#include "APCI.h"
+//#include "APCI.h"
 
-class APCI
-{
-	public: char start_field;
-	public: char ctrl_field_1;
-	public: char ctrl_field_2;
-	public: char ctrl_field_3;
-	public: char ctrl_field_4;
-};
+
+
+
+
